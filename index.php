be60eba06d907ad0388351f31b512c5242cb6748
Andre <andrelo@Ubuntu.myguest.virtualbox.org>
2024-03-07 21:37:28 -0600

    Agregando programa PHP

---
+++ index.php
@@ -0,0 +1,33 @@
+    <img src="paisaje.jpg" width="100" height="150" float:left>
+
+    <img src="paisaje.jpg" width="150" height="200" float:left>
+
+    <img src="paisaje.jpg" width="200" height="250" float:left>
+
+    <img src="paisaje.jpg" width="250" height="300" float:left>
+
+    <img src="paisaje.jpg" width="200" height="250" float:left>
+
+    <img src="paisaje.jpg" width="150" height="200" float:left>
+
+    <img src="paisaje.jpg" width="100" height="150" float:left>
+
+
+
+<?php
+    echo "<center><h1> HOLA MUNDO, soy Andrea López </h1></center>";
+?>
+
+    <img src="paisaje.jpg" width="100" height="150" float:left  style="transform:rotate(180deg); margin-bottom: 150;">
+
+    <img src="paisaje.jpg" width="150" height="200" float:left style="transform:rotate(180deg); margin-bottom: 100;">
+
+    <img src="paisaje.jpg" width="200" height="250" float:left style="transform:rotate(180deg); margin-bottom: 50;">
+
+    <img src="paisaje.jpg" width="250" height="300" float:left style="transform:rotate(180deg);">
+
+    <img src="paisaje.jpg" width="200" height="250" float:left style="transform:rotate(180deg); margin-bottom: 50px;">
+
+    <img src="paisaje.jpg" width="150" height="200" float:left style="transform:rotate(180deg); margin-bottom: 100px;">
+
+    <img src="paisaje.jpg" width="100" height="150" float:left style="transform:rotate(180deg); margin-bottom: 150px;">
